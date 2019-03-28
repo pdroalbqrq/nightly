48596a271acd38d9d64c3fc9f8e80a67f6c9cf54
pdroalbqrq <pdroalbqrq@gmail.com>
2019-03-28 00:26:37 -0300

    adicionando marcadores e box em baixo

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Dimensions, ScrollView } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-
+import { white } from 'ansi-colors';
+var customMapStyle = require('../../json/mapstyle.json');
 export default class Map extends Component {
     state = {
         region: null,
+        places: [
+            {
+                id: 1,
+                title: 'Igreja Santa Tereza',
+                description: 'Igreja para casamentos',
+                latlng: { latitude: -8.022007, longitude: -34.859251 }
+
+            },
+            {
+                id: 2,
+                title: 'Duetto Motel',
+                description: 'rsrs +18',
+                latlng: { latitude: -8.023675, longitude: -34.860146 }
+            },
+            {
+                id: 3,
+                title: 'CEMO (Centro de Educação Músical de Olinda)',
+                description: 'conservatório',
+                latlng: { latitude: -8.025802, longitude: -34.864383 }
+            }
+        ]
     }
 
     async componentDidMount() {
@@ -12,8 +34,8 @@ export default class Map extends Component {
             ({ coords: { latitude, longitude } }) => {
                 this.setState({
                     region: {
-                        latitude, 
-                        longitude, 
+                        latitude,
+                        longitude,
                         latitudeDelta: 0.0143,
                         longitudeDelta: 0.0134
                     }
@@ -28,30 +50,89 @@ export default class Map extends Component {
         )
     }
 
+
+
     render() {
-        const { region } =  this.state;
+        const { region } = this.state;
 
         return (
 
             <View style={styles.container}>
                 <MapView
-                    style={styles.map}
+                    showsPointsOfInterest={false}
+                    showsBuildings={false}
+                    style={styles.mapView}
                     region={region}
                     showsUserLocation
                     loadingEnabled
+                    customMapStyle={customMapStyle}
+
                 >
-                    {/* <Marker coordinate={this.state}/> */}
+                    {this.state.places.map(place => (
+                        <Marker
+                            key={place.id}
+                            coordinate={place.latlng}
+                        />
+                    ))}
                 </MapView>
+                <ScrollView
+                    style={styles.placesContainer}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    pagingEnabled 
+                    // onMomentumScrollEnd={e =>{
+                    //     const scrolled = e.nativeEvent.contentOffset.x;
+
+                    //     const place = (scrolled > 0)
+                    //     ? scrolled / Dimensions.get('window').width
+                    //     : 0;
+
+                    //     const { latitude, longitude } = this.state.places[place];
+
+                    //     this.mapView.animateToCoordinate({
+                    //         latitude,
+                    //         longitude
+                    //     });
+                    // }}
+                    >
+                    {this.state.places.map(place => (
+                        <View key={place.id} style={styles.places}>
+                            <Text>{place.title}</Text>
+                            <Text>{place.description}</Text>
+                        </View>
+                    ))}
+
+
+                </ScrollView>
             </View>);
+
     }
 }
 
+const { height, width } = Dimensions.get('window');
 const styles = StyleSheet.create({
     container: {
-        flex: 1
-    },
-    map: {
         flex: 1,
-        ...StyleSheet.absoluteFillObject,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end'
     },
+    mapView: {
+        flex: 1,
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        left: 0,
+        bottom: 0
+    },
+    placesContainer: {
+        width: '100%',
+        maxHeight: 200,
+    },
+    places: {
+        width: width - 40,
+        maxHeight: 200,
+        backgroundColor: '#FFF',
+        marginHorizontal: 20,
+        marginBottom: 20
+    }
 });
